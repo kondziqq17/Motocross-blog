fac105c35d61845cbac1f6169f109d1d9cdcade7
Konrad Pason <konrad.pason17@gmail.com>
2016-10-21 12:16:33 +0200

    Modified styles, fixed paperclip missing image

--- config/routes.rb
+++ config/routes.rb
@@ -7,4 +7,6 @@ Rails.application.routes.draw do
 
 	get '/home' => 'posts#home'
 	get '/posts/:id/:title' => 'posts#show', :as => :post_with_title
+	match '/contacts',     to: 'contacts#new', via: 'get'
+	resources "contacts", only: [:new, :create]
 end
